3d5d830c5a6c722f5089dd3804bd46b0c7772220
chaosbit <paul.c.busch@gmail.com>
2025-07-31 11:21:17 +0200

    wip

--- frontend/js/material-web-imports.js
+++ frontend/js/material-web-imports.js
@@ -4,9 +4,18 @@ import '../node_modules/@material/web/menu/menu-item.js';
 import '../node_modules/@material/web/button/filled-button.js';
 import '../node_modules/@material/web/button/text-button.js';
 import '../node_modules/@material/web/button/outlined-button.js';
+import '../node_modules/@material/web/button/elevated-button.js';
+import '../node_modules/@material/web/button/filled-tonal-button.js';
 import '../node_modules/@material/web/icon/icon.js';
 import '../node_modules/@material/web/iconbutton/icon-button.js';
 import '../node_modules/@material/web/divider/divider.js';
+import '../node_modules/@material/web/textfield/filled-text-field.js';
+import '../node_modules/@material/web/textfield/outlined-text-field.js';
+import '../node_modules/@material/web/select/filled-select.js';
+import '../node_modules/@material/web/select/outlined-select.js';
+import '../node_modules/@material/web/select/select-option.js';
+import '../node_modules/@material/web/switch/switch.js';
+import '../node_modules/@material/web/checkbox/checkbox.js';
 
 // Export for use in other modules if needed
 export { };
